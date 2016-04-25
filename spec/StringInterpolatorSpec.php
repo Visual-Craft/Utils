95291b326094264c04d04116ffe8ba85efc35490
Anton Bakai <inso@i.ua>
2016-04-25 18:07:52 +0300

    [StringInterpolator] added support for callables as 2nd argument for interpolate method

--- spec/StringInterpolatorSpec.php
+++ spec/StringInterpolatorSpec.php
@@ -2,29 +2,29 @@
 
 describe('VisualCraft\\Utils\\StringInterpolator\\StringInterpolator', function() {
     $samples = [
-        ['test test test', 'test test test', []],
-        ['test $foo test', 'test boo test', ['foo']],
-        [' $foo $goo ', ' boo fff ', ['foo', 'goo']],
-        ['$foo $goo $foo', 'boo fff boo', ['foo', 'goo']],
-        ['${foo}', 'boo', ['foo']],
-        ['${foo}${goo}', 'boofff', ['foo', 'goo']],
-        ['ff${foo}test', 'ffbootest', ['foo']],
-        ['\$foo', '$foo', []],
-        ['${foo}\$foo', 'boo$foo', ['foo']],
-        ['$fö', 'value', ['fö']],
-        ['${fö}', 'value', ['fö']],
-        ['${go⤗o}', '${go⤗o}', []],
-        ['test$%&test', 'test$%&test', []],
-        ['test \\\\$foo test \\\\${goo}', 'test \boo test \fff', ['foo', 'goo']],
-        ['\\\\\\$foo', '\\\\$foo', []],
-        ['\\\\\\\\$foo', '\\\\\\boo', ['foo']],
+        ['test test test', 'test test test', 'test test test', []],
+        ['test $foo test', 'test boo test', 'test foo_var test', ['foo']],
+        [' $foo $goo ', ' boo fff ', ' foo_var goo_var ', ['foo', 'goo']],
+        ['$foo $goo $foo', 'boo fff boo', 'foo_var goo_var foo_var', ['foo', 'goo']],
+        ['${foo}', 'boo', 'foo_var', ['foo']],
+        ['${foo}${goo}', 'boofff', 'foo_vargoo_var', ['foo', 'goo']],
+        ['ff${foo}test', 'ffbootest', 'fffoo_vartest', ['foo']],
+        ['\$foo', '$foo', '$foo', []],
+        ['${foo}\$foo', 'boo$foo', 'foo_var$foo', ['foo']],
+        ['$fö', 'value', 'fö_var', ['fö']],
+        ['${fö}', 'value', 'fö_var', ['fö']],
+        ['${go⤗o}', '${go⤗o}', '${go⤗o}', []],
+        ['test$%&test', 'test$%&test', 'test$%&test', []],
+        ['test \\\\$foo test \\\\${goo}', 'test \boo test \fff', 'test \foo_var test \goo_var', ['foo', 'goo']],
+        ['\\\\\\$foo', '\\\\$foo', '\\\\$foo', []],
+        ['\\\\\\\\$foo', '\\\\\\boo', '\\\\\\foo_var', ['foo']],
     ];
 
     beforeEach(function () {
         $this->interpolator = new \VisualCraft\Utils\StringInterpolator\StringInterpolator();
     });
 
-    describe('->interpolate()', function() use ($samples) {
+    describe('->interpolate() with array', function() use ($samples) {
         beforeEach(function () {
             $this->interpolate = function ($subject) {
                 return $this->interpolator->interpolate($subject, [
@@ -51,6 +51,22 @@ describe('VisualCraft\\Utils\\StringInterpolator\\StringInterpolator', function(
         }
     });
 
+    describe('->interpolate() with callable', function() use ($samples) {
+        beforeEach(function () {
+            $this->interpolate = function ($subject) {
+                return $this->interpolator->interpolate($subject, function ($name) {
+                    return $name . '_var';
+                });
+            };
+        });
+
+        foreach ($samples as $sample) {
+            it("should return '{$sample[2]}' for '{$sample[0]}'", function() use ($sample) {
+                expect($this->interpolate($sample[0]))->toBe($sample[2]);
+            });
+        }
+    });
+
     describe('->getNames()', function() use ($samples) {
         beforeEach(function () {
             $this->getNames = function ($subject) {
@@ -59,9 +75,9 @@ describe('VisualCraft\\Utils\\StringInterpolator\\StringInterpolator', function(
         });
 
         foreach ($samples as $sample) {
-            $return = json_encode($sample[2]);
+            $return = json_encode($sample[3]);
             it("should return '{$return}' for '{$sample[0]}'", function() use ($sample) {
-                expect($this->getNames($sample[0]))->toBe($sample[2]);
+                expect($this->getNames($sample[0]))->toBe($sample[3]);
             });
         }
     });
--- src/StringInterpolator/StringInterpolator.php
+++ src/StringInterpolator/StringInterpolator.php
@@ -16,15 +16,29 @@ class StringInterpolator
 
     /**
      * @param string|string[] $subject
-     * @param array $variables
+     * @param array|callable $variablesOrCallable
      *
      * @return string|string[]
      *
      * @throws MissingVariableException
      */
-    public function interpolate($subject, $variables)
+    public function interpolate($subject, $variablesOrCallable)
     {
-        return preg_replace_callback($this->regexp, function ($matches) use ($variables) {
+        $variables = null;
+        $callable = null;
+
+        if (is_array($variablesOrCallable)) {
+            $variables = $variablesOrCallable;
+        } elseif (is_callable($variablesOrCallable)) {
+            $callable = $variablesOrCallable;
+        } else {
+            throw new \InvalidArgumentException(sprintf(
+                "Argument 'variablesOrCallable' should be array or callable but '%s' is given.",
+                is_object($variablesOrCallable) ? get_class($variablesOrCallable) : gettype($variablesOrCallable)
+            ));
+        }
+
+        return preg_replace_callback($this->regexp, function ($matches) use ($variables, $callable) {
             $escape = isset($matches['escape']) ? $matches['escape'] : '';
             $escapeLength = $escape !== '' ? strlen($escape) : 0;
 
@@ -32,11 +46,17 @@ class StringInterpolator
                 return substr($matches[0], 1);
             }
 
-            if (isset($variables[$matches['key']])) {
-                return substr($escape, 0, $escapeLength - 1) . $variables[$matches['key']];
-            }
+            $prefix = substr($escape, 0, $escapeLength - 1);
 
-            throw new MissingVariableException(sprintf("Missing variable '%s'.", $matches['key']));
+            if ($callable) {
+                return $prefix . $callable($matches['key']);
+            } else {
+                if (isset($variables[$matches['key']])) {
+                    return $prefix . $variables[$matches['key']];
+                }
+
+                throw new MissingVariableException(sprintf("Missing variable '%s'.", $matches['key']));
+            }
         }, $subject);
     }
 
